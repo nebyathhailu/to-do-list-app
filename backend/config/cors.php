c5419da01c550420df943bad5323efed2405e940
Nebyat <nebyathhailu@gmail.com>
2026-03-04 00:26:26 +0300

    fix: remove trailing slash from CORS allowed origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://to-do-list-app-two-eta.vercel.app/',
+        'https://to-do-list-app-two-eta.vercel.app',
         
         ],
 
